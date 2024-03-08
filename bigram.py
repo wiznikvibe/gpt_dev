2855674557de583c3a5105136d37562045531391
wiznikvibe <nikhilshetty439@gmail.com>
2024-03-08 20:49:21 +0530

    Attention Module

--- bigram.py
+++ bigram.py
@@ -11,7 +11,7 @@ eval_interval = 300
 learning_rate = 1e-2
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-
+n_embd = 32
 # --------------------- # 
 
 torch.manual_seed(1337)
@@ -55,7 +55,7 @@ def estimate_loss():
         for  k in range(eval_iters):
             X, Y = get_batch(split)
             logits, loss = model(X, Y)
-            losses[k] = loss.items()
+            losses[k] = loss.item()
         out[split] = losses.mean()
     model.train()
 
@@ -64,23 +64,26 @@ def estimate_loss():
 
 class BigramLanguageModel(nn.Module):
 
-  def __init__(self, vocab_size):
-
+  def __init__(self):
     super().__init__()
     # lookup table 
     self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+    # self.position_embedding_table = nn.Embedding(block_size, n_embd)
+    # self.lm_head = nn.Linear(n_embd, vocab_size)
 
 
   def forward(self, idx, targets=None):
+    # B, T = idx.shape 
+
     # Score for next character sequence 
+    # tok_emb = self.token_embedding_table(idx)
+    # pos_emb = self.position_embedding_table(torch.arange(T, device=device))
+    # x = tok_emb + pos_emb
     logits = self.token_embedding_table(idx)
 
     if targets is None:
-
       loss = None
-
     else:
-
       B,T,C = logits.shape
       logits = logits.view(B*T, C)
       targets = targets.view(B*T)
@@ -92,17 +95,20 @@ class BigramLanguageModel(nn.Module):
   def generate(self, idx, max_new_tokens):
 
     for _ in range(max_new_tokens):
-      logits, loss = self(idx)
-
-      logits = logits[:, -1, :]
-      probs = F.softmax(logits, dim=-1)
-
-      idx_next = torch.multinomial(probs, num_samples=1)
-      idx = torch.cat((idx, idx_next), dim=1)
-
+            # get the predictions
+            logits, loss = self(idx)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
     return idx
 
-model = BigramLanguageModel(vocab_size)
+
+model = BigramLanguageModel()
 m = model.to(device)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
@@ -125,3 +131,6 @@ for iter in range(max_iters):
 # generate from the model
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
 print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
+
+
+
